6c71be3f6f0f6885b1136b7fa16272a5be60f2a7
v-lane <victoria.lane.m@gmail.com>
2024-02-27 11:51:26 -0500

    Add comments for clarity

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,9 @@
 
 
 $(document).ready(function() {
-  $("#tweet-text").on('input', function(event) {
+
+  // on input in id tweet-text, updates counter for tweet-text with remaining characters
+  $("#tweet-text").on('input', function() {
     let valueLength = $(this).val().length;
     let charRemaining = 140 - (valueLength)
     $(this).parents('.new-tweet').find('.counter').text(charRemaining)
