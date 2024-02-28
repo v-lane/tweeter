71efde4b07e18fe18af321c14b9788eb944bd084
v-lane <victoria.lane.m@gmail.com>
2024-02-28 15:13:36 -0500

    Add tweet data files to render on page load

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,6 @@
 
 $(document).ready(function() {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   // input value in milliseconds, returns value in days (rounded down to nearest day)
   const msToDay = function(ms) {
     const day = Math.floor(ms / 1000 / 60 / 60 / 24);
@@ -71,7 +44,7 @@ $(document).ready(function() {
     return tweet;
   };
 
-  // given all tweets data, as array of objects, appends all tweets as individual tweet elements to main element
+  // given all tweets data, as array of objects, prepends all tweets as individual tweet elements to main element
   const renderTweet = function(tweetData) {
     for (const tweet of tweetData) {
       const $tweet = createTweetElement(tweet);
@@ -79,16 +52,31 @@ $(document).ready(function() {
     }
   };
 
-  renderTweet(data);
+  // gets tweet data from '/tweets/' and calls renderTweet function
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      success: function(res) {
+        renderTweet(res);
+      }
+    })
+  }
+
+  loadTweets();
+
 
   $('.new-tweet form').on('submit', function(event) {
     event.preventDefault();
     const textSerialized = $(this).serialize();
 
     $.ajax({
-      type: "POST",
+      method: "POST", 
       url: "/tweets",
       data: textSerialized,
+      success: function() {
+        $('#tweet-text').val('');
+      }
     });
   });
 
