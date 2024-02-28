2e6cd3ebdc34200fddbf16ca90b8cd5cc70c53eb
v-lane <victoria.lane.m@gmail.com>
2024-02-28 11:06:52 -0500

    Add dynamic tweet framework

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,61 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+
+  // input value in milliseconds, returns value in days (rounded down to nearest day)
+  const msToDay = function(ms) {
+    const day = Math.floor(ms / 1000 / 60 / 60 / 24)
+    return day;
+  }
+
+  // given value in milliseconds, returns number of days ago that time was from now (rounded down to nearest day)
+  const daysAgo = function(ms) {
+    const today = new Date();
+    const timeAgo = today - ms;
+    return msToDay(timeAgo)
+  }
+
+
+  const createTweetElement = function(tweetData) {
+    const tweet = $(`
+    <article class="tweet">
+    <header>
+      <div class="userInfo">
+        <img src=${tweetData.user.avatars}>
+        <h2>${tweetData.user.name}</h2>
+      </div>
+      <h3>${tweetData.user.handle}</h3>
+    </header>
+    <p>${tweetData.content.text}</p>
+    <footer>
+      <time>${daysAgo(tweetData.created_at)} days ago</time>
+      <div class="interactions">
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>`
+    );
+    return tweet;
+  };
+
+  const $tweet = createTweetElement(tweetData);
+
+  $('main').append($tweet);
+
+});
